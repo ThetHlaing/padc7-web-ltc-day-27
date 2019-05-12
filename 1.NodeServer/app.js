79e4848466793824cf369cb6ee8521fe255585d9
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-05-12 18:19:01 +0630

    EOD

--- 1.NodeServer/app.js
+++ 1.NodeServer/app.js
@@ -5,7 +5,7 @@ const app = express();
 const sql = require('./utilities/mysql');
 
 const fruitsRoute = require('./routes/fruits');
-
+const apiRoute = require('./routes/api');
 const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
@@ -13,8 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(__dirname + "/static"));
 app.use('/fruits', fruitsRoute);
-
-
+app.use('/api/',apiRoute);
 
 app.get("/", (req, res) => {
     res.render('home');
