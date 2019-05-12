19a583dc58e6751bdb5af8145dc7a1ce3dcd1750
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-05-12 14:51:24 +0630

    Initial Commit

---
+++ 1.NodeServer/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const sql = require('./utilities/mysql');
+
+const fruitsRoute = require('./routes/fruits');
+
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(__dirname + "/static"));
+app.use('/fruits', fruitsRoute);
+
+
+
+app.get("/", (req, res) => {
+    res.render('home');
+})
+
+app.listen(3000);
